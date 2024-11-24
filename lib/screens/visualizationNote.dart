e0f29c0c87e00a670447043795b2bf2dc73c6f20
Chang <xiangxiang3451@gmail.com>
2024-11-25 02:01:47 +0700

    Visualization

--- lib/screens/visualizationNote.dart
+++ lib/screens/visualizationNote.dart
@@ -1,9 +1,7 @@
 import 'dart:convert';
-
 import 'package:emmo/authentication/authentication_service.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 
 class Visualizationnote extends StatefulWidget {
   const Visualizationnote({super.key});
