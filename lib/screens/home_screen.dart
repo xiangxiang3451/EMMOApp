50c65e5b876ae6b01fa97f091ea2a96346fa091e
Chang <xiangxiang3451@gmail.com>
2024-12-08 02:32:45 +0700

    add Sidebar

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,10 +1,10 @@
+import 'package:flutter/material.dart';
 import 'package:emmo/screens/calendar_screen.dart';
 import 'package:emmo/screens/emotionChatPage.dart';
 import 'package:emmo/screens/mood_screen.dart';
 import 'package:emmo/screens/setting_screen.dart';
 import 'package:emmo/screens/driftBottlePage.dart';
 import 'package:emmo/screens/visualizationNote.dart';
-import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -16,13 +16,16 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   int _selectedIndex = 0;
 
+  // 创建一个 GlobalKey，用来控制 Scaffold
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+
   // 页面选项列表
   static final List<Widget> _widgetOptions = <Widget>[
     const CalendarScreen(),
-    EmotionChatPage(), 
-    const Visualizationnote(), 
-    DriftBottlePage(), 
-    const SettingScreen()
+    EmotionChatPage(),
+    const Visualizationnote(),
+    DriftBottlePage(),
+    const SettingScreen(),
   ];
 
   // 页面切换方法
@@ -30,24 +33,21 @@ class _HomeScreenState extends State<HomeScreen> {
     setState(() {
       _selectedIndex = index;
     });
-
-    // // 判断点击的是否是 "语言设置" 项，如果是，显示 MoodScreen
-    // if (index == 3) {
-    //   _openMoodScreen();
-    // }
   }
 
   // 打开MoodScreen页面的方法
   void _openMoodScreen() {
     Navigator.push(
       context,
-      MaterialPageRoute(builder: (context) => const MoodScreen()), // 跳转到MoodScreen
+      MaterialPageRoute(
+          builder: (context) => const MoodScreen()), // 跳转到MoodScreen
     );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: _scaffoldKey, // 给 Scaffold 设置 GlobalKey
       appBar: AppBar(
         automaticallyImplyLeading: false, // 禁用默认的返回按钮
         centerTitle: true,
@@ -63,11 +63,20 @@ class _HomeScreenState extends State<HomeScreen> {
         ),
         title: const Text('EMMO'),
         leading: IconButton(
-          icon: const Icon(Icons.mode), // 使用铅笔
+          icon: const Icon(Icons.mode), // 使用铅笔图标
           onPressed: _openMoodScreen, // 用户点击时打开 MoodScreen 页面
         ),
+        actions: [
+          // 添加设置按钮到右侧
+          IconButton(
+            icon: const Icon(Icons.settings), // 设置按钮的图标
+            onPressed: () {
+              // 使用 _scaffoldKey 打开侧边栏
+              _scaffoldKey.currentState?.openDrawer();
+            },
+          ),
+        ],
       ),
-
       body: Center(
         child: _widgetOptions.elementAt(_selectedIndex), // 根据选中的索引显示相应页面
       ),
@@ -102,6 +111,43 @@ class _HomeScreenState extends State<HomeScreen> {
         onTap: _onItemTapped,
         type: BottomNavigationBarType.fixed, // 固定模式，确保未选中的图标保持原样
         iconSize: 20,
+      ),
+      drawer: Container(
+        width: MediaQuery.of(context).size.width / 2,
+        child: Drawer(
+          backgroundColor: const Color(0xFF62D2A2),
+          child: ListView(
+            padding: const EdgeInsets.all(22.0),
+            children: [
+              ListTile(
+                title: const Text('选项 1',style: TextStyle(color: Colors.white),),
+                onTap: () {
+                  Navigator.pop(context); // 关闭 Drawer
+                },
+              ),
+              ListTile(
+                title: const Text('选项 2',style: TextStyle(color: Colors.white),),
+                onTap: () {
+                  Navigator.pop(context); // 关闭 Drawer
+                },
+              ),
+              const Divider(), // 分割线
+              // 设置项
+              ListTile(
+                title: const Text('设置',style: TextStyle(color: Colors.white),),
+                onTap: () {
+                  Navigator.pop(context); // 关闭 Drawer
+                  // 展示设置内容，跳转到设置页面
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const SettingScreen()),
+                  );
+                },
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
